d338bbe214bd3da3250adba7ee4511ef4dbf4c54
bymaslol <53442778+bymaslol@users.noreply.github.com>
2024-04-29 17:14:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let history = [];
 // i change this depending if im testing or not
 // let api_url = 'http://127.0.0.1:5000/v1/chat/completions';
 
-const cloudflare_url = 'https://tri-matched-ink-preservation.trycloudflare.com';
+const cloudflare_url = 'https://reflects-sue-trend-static.trycloudflare.com';
 const api_url = `${cloudflare_url}/v1/chat/completions`;
 // user context info
 const userHeight = "5'9";
